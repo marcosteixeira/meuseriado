366513f356c439c7953a849357120fab4be23c8b
Marcos Teixeira <m.viniteixeira@gmail.com>
2013-08-07 13:50:32 -0300

    Salvando imagens no deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,8 +10,6 @@ set :rails_env, :production
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 server "192.241.234.24", :app, :web, :db, :primary => true
-set :deploy_via, :copy
-set :copy_exclude, [".git/*", ".svn/*", "app/images/series/*"]
 set :stages, ["production"]
 set :default_stage, "production"
 
@@ -22,11 +20,16 @@ namespace :deploy do
         run "cd #{current_path} && touch tmp/restart.txt"
     end
 end
+before :deploy, 'deploy:copia_imagens'
 after :deploy, 'deploy:database'
 namespace :deploy do
     task :database, :roles => :app do
         run "cp #{deploy_to}shared/database.yml #{current_path}/config/"
     end
+    task :copia_imagens do
+        run "mv #{current_path}/app/assets/images/series/ #{deploy_to}shared/"
+    end
+
 end
 
 namespace :bundle do
@@ -35,6 +38,9 @@ namespace :bundle do
   task :install do
     run "cd #{current_path} && bundle install  --deployment && rake db:migrate RAILS_ENV=\"production\""
   end
-
+  task :restaura_imagens do 
+    run "cp -r  #{deploy_to}shared/series/ #{current_path}/app/assets/images/ && rm -rf #{deploy_to}shared/series/ "
+  end
 end
 before "deploy:restart", "bundle:install"
+after "bundle:install", "bundle:restaura_imagens"
