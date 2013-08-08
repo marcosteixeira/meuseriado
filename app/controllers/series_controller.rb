679dd4c3731b9f75d600a1f153cc037e6b8f4dbd
Marcos Teixeira <m.viniteixeira@gmail.com>
2013-08-08 16:11:13 -0300

    Tentativa de correção para séries com pesquisa grande

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -7,6 +7,11 @@ class SeriesController < ApplicationController
   
   require 'open-uri'  
   def index
+  
+    if params['id_serie_search']
+      buscar_series
+    end
+    
     @series||= Serie.order(:nome)
     @iniciais = cria_array_iniciais(@series)
   end
