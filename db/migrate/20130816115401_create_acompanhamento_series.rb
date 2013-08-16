8a2b20067cf73a8a99259e47259b003c1d8a7096
Marcos Teixeira <m.viniteixeira@gmail.com>
2013-08-16 19:15:11 -0300

    Melhoria na tabela

--- db/migrate/20130816115401_create_acompanhamento_series.rb
+++ db/migrate/20130816115401_create_acompanhamento_series.rb
@@ -1,15 +1,13 @@
 class CreateAcompanhamentoSeries < ActiveRecord::Migration
   def change
-    create_table :acompanhamento_series, :id => false do |t|
-      t.integer :serie_id, :null => false
-      t.integer :user_id, :null => false
+    create_table :acompanhamento_series do |t|
+      t.integer :avaliacao_id, :null => false
       t.boolean :ativa
       t.boolean :finalizada
       t.boolean :geladeira
       t.timestamps
     end
-    add_foreign_key(:acompanhamento_series, :series)
-    add_foreign_key(:acompanhamento_series, :users)
-    add_index :acompanhamento_series, [:serie_id,:user_id], unique: true
+    add_foreign_key(:acompanhamento_series, :avaliacoes)
+    add_index :acompanhamento_series, :avaliacao_id, unique: true
   end
 end
