e8a705e14985db117a1fefb0105c897747d46731
Marcos Teixeira <m.viniteixeira@gmail.com>
2013-10-19 13:25:00 -0300

    Melhoria na estética dos comentários

--- app/models/commontator/comment.rb
+++ app/models/commontator/comment.rb
@@ -56,10 +56,15 @@ module Commontator
 
     def timestamp
       config = thread.config
-      "#{config.comment_create_verb_past.capitalize} em " + \
-        created_at.strftime(config.timestamp_format) + \
-        (is_modified? ? " | #{config.comment_edit_verb_past} em " + \
-          updated_at.strftime(config.timestamp_format) : '')
+
+      if is_modified?
+        "#{config.comment_edit_verb_past.capitalize} em " + \
+        updated_at.strftime(config.timestamp_format)
+      else
+        "#{config.comment_create_verb_past.capitalize} em " + \
+        created_at.strftime(config.timestamp_format)
+      end
+
     end
 
     ##################
