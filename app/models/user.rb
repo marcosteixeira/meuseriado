d89afd22ecb9ddbb1752213f44dee2f9bc64c62a
Marcos Teixeira <m.viniteixeira@gmail.com>
2013-11-04 10:42:59 -0200

    Ordenação de séries

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
                     :url => "/images/series/:id/:filename"
 
   has_many :avaliacoes, :dependent => :delete_all, :order => 'id DESC'
-  has_many :series_vistas, :through => :avaliacoes, :source => :acompanhamento_serie
+  has_many :series_vistas, :through => :avaliacoes, :source => :acompanhamento_serie, :order => 'avaliacoes.updated_at DESC'
 
   def series
     Serie.find_by_sql("SELECT  `series`.* FROM `series` INNER JOIN `avaliacoes` ON `series`.`id` = `avaliacoes`.`avaliavel_id` WHERE `avaliacoes`.`user_id` = #{self.id} AND `avaliacoes`.`avaliavel_type` = 'Serie'  ORDER BY id DESC")
