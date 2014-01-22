1aee1e96a5a463507128064701faadafc67e0251
Marcos Teixeira <m.viniteixeira@gmail.com>
2014-01-22 08:03:01 -0200

    Ruffus

--- config/initializers/ruffus.rb
+++ config/initializers/ruffus.rb
@@ -1,9 +1,19 @@
 require 'rufus-scheduler'
 
+
 scheduler = Rufus::Scheduler.new
 
-scheduler.cron '00 3 * * *' do
-  Serie.updater_bot
+def programa_cron
+  scheduler.cron '00 3 * * *' do
+    Serie.updater_bot
+  end
+  scheduler.join
 end
 
-scheduler.join
+if defined?(PhusionPassenger)
+  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+    if forked
+      programa_cron
+    end
+  end
+end
